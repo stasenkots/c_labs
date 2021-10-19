d97cf8c6ade18b3ebf2742f1571939e20f106005
stasenkots <tstasenko2@gmail.com>
2021-10-19 17:26:27 +0300

    Done lesson 3

---
+++ cycles/lesson.cpp
@@ -0,0 +1,35 @@
+//
+// Created by kotozavrina on 19/10/2021.
+//
+
+#include "iostream"
+
+int main() {
+//    ///while and do while
+//    int i = 0;
+//    while (i < 3) {
+//        std::cout << i << std::endl;
+//        i += 1;
+//    }
+//
+//    i = 0;
+//    do {
+//        std::cout << i << std::endl;
+//        i += 1;
+//    } while (i < 3);
+//
+//    ///for
+//
+//    for (int i = 0; i < 3; ++i) {
+//        std::cout << i << std::endl;
+//    }
+
+//    for (int i = 0; i > -5; --i) {
+//        std::cout << i << std::endl;
+//    }
+
+//   for (int i = 0,j = 5; i < j; ++i,--j) {
+//        std::cout << i << " " << j << std::endl;
+//   }
+
+}
